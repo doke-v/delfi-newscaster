292335538a720f85cdb0763772864b7a41b4fac2
tuhnik <huntlarsen@gmail.com>
2017-12-01 15:33:18 +0200

    comments added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var news = []
 
 module.exports = function delfi(callback, interval) {
     interval = interval || 120000
-    callback = callback || console.log
+    
 
     var request = require("request"),
         cheerio = require("cheerio"),
@@ -25,15 +25,18 @@ module.exports = function delfi(callback, interval) {
                 url: url,
                 comments: comments
             }
-
+//keep temporary array short
             while (news.length > 10) {
                 news.shift()
             }
+//push first url to array            
             if (news.length < 1) news.push(url)
-
+//check if current url exists in array
             if (news.indexOf(url) >= 0) {
+//if yes, then stop               
                 return;
             } else {
+//if no, then push to array and send object to user                
                 news.push(url)
                 callback(null, uudis)
             }
